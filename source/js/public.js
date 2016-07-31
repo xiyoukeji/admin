9e6c53555945497a0acc99ee7fc8eea18626035d
chendind <3110100527@zju.edu.cn>
2016-07-31 20:19:37 +0800

    我了个大擦，给tinymce做了个本地图片上传

--- source/js/public.js
+++ source/js/public.js
@@ -6,10 +6,11 @@ var ADMIN_CONFIG = {
 	"leftSelector":"#admin_left"
 };
 $(function(){
-adminInit();
-function adminInit(){
-	eventBind();
-}
+    adminInit();
+    function adminInit(){
+    	eventBind();
+    }
+
 });
 function eventBind(){
 	// 页面刷新 hash变化时的处理
@@ -42,9 +43,55 @@ function eventBind(){
     		
     	}
     });
-    // $(contentSelector+" .leftmenu>div").bind('mouse',function(){
-
-    // });
+}
+function uiComponentEventBind(){
+    $.each($("[data-autoComplete]"),function(){
+        $(this).bind('input',function(){
+            var $this = $(this);
+            var keyword = $.trim($this.val());
+            var params = $this.attr("data-params");
+            var regx = /\<js\>([\s\S^(<\/js\>)]+)\<\/js\>/;
+            regx.exec(params);
+            if(keyword!=""){
+                timeout&&clearTimeout(timeout);
+                timeout = setTimeout(function(){
+                    $.ajax({
+                        url: url,
+                        type: type,
+                        data:{
+                            "key":keyword
+                        },
+                        success: function(data){
+                            switch(data.state){
+                                case "0":
+                                    $this.siblings(".searchResult").empty();
+                                    var school=data.school;
+                                    if(school.length>0){
+                                        for (var i = 0, length = school.length; i < length; i++){
+                                            var orderhtml=$('<li>'+
+                                                '<a href="'+baseROOT+'/search.html?key='+school[i]+'">'+school[i]+'</a>'+
+                                                '</li>');
+                                            $this.siblings(".searchResult").append(orderhtml);
+                                        }
+                                        $this.siblings(".searchResult").show();
+                                    }
+                                    else{
+                                        $this.siblings(".searchResult").hide();
+                                    }
+                                break;
+                                default: 
+                                    $this.siblings(".searchResult").hide();
+                                break;
+                            }
+                        },
+                        error: function(){
+                                
+                        }
+                    });
+                }, 300);
+            }
+        })
+    });
 }
 function windowReset(){
 	var h =  $(window).height()-$(ADMIN_CONFIG.headerSelector).height();
@@ -56,7 +103,7 @@ function loadContent() {
         hash = "#/"+ADMIN_CONFIG.homePage;
     }
     $(ADMIN_CONFIG.contentSelector).load(hash.split("/")[1], function(){
-        
+        uiComponentEventBind();
     });
 }
 

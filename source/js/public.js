b72c313c2c5bce5fea509e42669a9cbc2e6fe752
chendind <3110100527@zju.edu.cn>
2016-08-02 11:55:10 +0800

    完成 表单和编辑器 > 输入自动补齐

--- source/js/public.js
+++ source/js/public.js
@@ -47,67 +47,78 @@ function eventBind(){
 function uiComponentEventBind(){
     $.each($("[data-autoComplete]"),function(){
         var $this = $(this);
-        var _params = params = $this.attr("data-params");
+        var $input = $(this).children('input');
+        var _params = params = $this.attr('data-params');
         var regx = /<js>(.*?)<\/js>/g;
         do{
             var matchArr = regx.exec(params);
             matchArr&&(_params = _params.replace(/<js>.*?<\/js>/,eval(matchArr[1])));
         }while(regx.lastIndex);
         _params = JSON.parse(_params);
-        if(!$this.siblings('.admin_ui_input_autoCompleteBox').length){
-            $this.after('<div class="admin_ui_input_autoCompleteBox"></div>');
+        if(!$input.siblings('.admin_ui_input_autoComplete_selectBox').length){
+            $input.after('<div class="admin_ui_input_autoComplete_selectBox"></div>');
         }
-        var autoCompleteBox = $this.siblings(".admin_ui_input_autoCompleteBox");
-        $this.bind('input',function(){
-            var keyword = $.trim($this.val());
+        var selectBox = $input.siblings(".admin_ui_input_autoComplete_selectBox");
+        $input.bind('input',function(){
+            var keyword = $.trim($input.val());
             var timeout;
             if(keyword!=""){
                 timeout&&clearTimeout(timeout);
                 timeout = setTimeout(function(){
-                    if($this.val()){
-                        autoCompleteBox.empty().hide();
-                        $.ajax(_params).done(function(data){
-                            if(data.state == 0){
-                                var textArr = data.data;
-                                if(textArr.length > 0){
-                                    for(var i = 0, length = textArr.length; i < length; i++){
-                                        var line = $('<div class="line">'+
-                                                        textArr[i]+
-                                                    '</div>');
-                                        line.bind('click',function(){
-                                            console.log($(this).text())
-                                            // if($this.siblings('.admin_ui_input_tagBox').length){
-                                            //     $this.before('<div class="admin_ui_input_tagBox"></div>');
-                                            // }
-                                            // var tagBox = $this.siblings('.admin_ui_input_tagBox');
-                                            // for(var k = 0, length = tagBox.children('.admin_ui_colorLabel').length; k < length; k++){
-                                            //     if(tagBox.children('.admin_ui_colorLabel').eq(k).text() == $(this).text()){
-                                            //         break;
-                                            //     }
-                                            //     if(k == length-1){
-                                            //         tagBox.append('<div class="admin_ui_colorLabel">'+$(this).text()+'</div>');
-                                            //     }
-                                            // }
-                                        });
-                                        autoCompleteBox.append(line);
-                                    }
-                                    autoCompleteBox.show();
+                    selectBox.empty().hide();
+                    $.ajax(_params).done(function(data){
+                        if(data.state == 0){
+                            var textArr = data.data;
+                            if(textArr.length > 0){
+                                for(var i = 0, length = textArr.length; i < length; i++){
+                                    var line = $('<div class="line">'+
+                                                    textArr[i]+
+                                                '</div>');
+                                    line.bind('click',function(){
+                                        if(!$this.siblings('.admin_ui_input_tagBox').length){
+                                            $this.before('<div class="admin_ui_input_tagBox"></div>');
+                                            $this.siblings('.admin_ui_input_tagBox').on('click','.admin_ui_colorLabel',function(){
+                                                $(this).remove();
+                                            });
+                                        }
+                                        var tagBox = $this.siblings('.admin_ui_input_tagBox');
+                                        var length = length = tagBox.children('.admin_ui_colorLabel').length;
+                                        if(length){
+                                            for(var k = 0; k < length; k++){
+                                                if(tagBox.children('.admin_ui_colorLabel').eq(k).text() == $(this).text()){
+                                                    break;
+                                                }
+                                                if(k == length-1){
+                                                    tagBox.append('<div class="admin_ui_colorLabel">'+$(this).text()+'</div>');
+                                                }
+                                            }
+                                        }
+                                        else{
+                                            tagBox.append('<div class="admin_ui_colorLabel">'+$(this).text()+'</div>');
+                                        }  
+                                    });
+                                    selectBox.append(line);
                                 }
-                                else{
-                                    var line = '<div class="line" onclick="return false">'+
-                                                    '无搜索结果'+
-                                                '</div>';
-                                    autoCompleteBox.append(line);
-                                }  
+                                selectBox.show();
                             }
-                        }).fail(function(data){
-                            console.log(data);
-                        });
-                    }
+                            else{
+                                var line = '<div class="line" onclick="return false">'+
+                                                '无搜索结果'+
+                                            '</div>';
+                                selectBox.append(line);
+                            }  
+                        }
+                    }).fail(function(data){
+                        console.log(data);
+                    });
                 }, 500);
+            }
+            else{
+                selectBox.empty().hide();
             }
         })
     });
+    
 }
 function windowReset(){
 	var h =  $(window).height()-$(ADMIN_CONFIG.headerSelector).height();
